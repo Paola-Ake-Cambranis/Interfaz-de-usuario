aecdd56b8c59c34795347531d7a9d20a4a77614b
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2024-02-01 21:40:36 -0600

    Version 1.0

---
+++ Ventana.java
@@ -0,0 +1,16 @@
+
+package mycompany.ejercicios;
+
+import javax.swing.*;
+public class Ventana extends JFrame{
+    public Ventana(){
+        setTitle("Lista de pendientes"); 
+        setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        iniciarComponentes();
+        
+    }
+    
+    
+}
