9022963d94dd619d152474abbb17398d0e39f1fb
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2024-02-01 22:43:09 -0600

    Version 1.3

--- Ventana.java
+++ Ventana.java
@@ -4,7 +4,7 @@ package mycompany.ejercicios;
 import javax.swing.*;
 public class Ventana extends JFrame{
     public Ventana(){
-        setTitle("Lista de compras"); 
+        setTitle("Notas"); 
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -13,15 +13,20 @@ public class Ventana extends JFrame{
     }
     
     private void iniciarComponentes(){
-        
         JPanel panel = new JPanel();
         panel.setLayout(null);
         getContentPane().add(panel);
-        JLabel etiqueta = new JLabel("Lista de compras");
+        
+        JLabel etiqueta = new JLabel("Notas");
+        etiqueta.setBounds(200,10,150,50);
         panel.add(etiqueta);
         
-        JTextField texto = new JTextField();
-        texto.setBounds(100,100, 50,50);
+        JTextArea texto = new JTextArea();
+        texto.setBounds(40,50,400,300);
         panel.add(texto);
+        
+        JButton boton = new JButton("Guardar");
+        boton.setBounds(190, 380, 100,30);
+        panel.add(boton);
     }
 }
