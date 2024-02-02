175df90b1f26a5cd4c37f446be911e8581972b42
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2024-02-01 22:28:42 -0600

    Version 1.2

--- Ventana.java
+++ Ventana.java
@@ -4,7 +4,7 @@ package mycompany.ejercicios;
 import javax.swing.*;
 public class Ventana extends JFrame{
     public Ventana(){
-        setTitle("Lista de pendientes"); 
+        setTitle("Lista de compras"); 
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -15,10 +15,13 @@ public class Ventana extends JFrame{
     private void iniciarComponentes(){
         
         JPanel panel = new JPanel();
-        
+        panel.setLayout(null);
         getContentPane().add(panel);
-        
-        JLabel etiqueta = new JLabel("Lista de pendientes");
+        JLabel etiqueta = new JLabel("Lista de compras");
         panel.add(etiqueta);
+        
+        JTextField texto = new JTextField();
+        texto.setBounds(100,100, 50,50);
+        panel.add(texto);
     }
 }
